aeb14c56256237e1e60a7477ccbd6349560e7f4f
wbyoung <wbyoung@16795141-6145-0410-a428-534aef879a8c>
2007-08-12 02:06:18 +0000

    moved sparkle plus code into framework for easy access

--- Source/FSFramework.h
+++ Source/FSFramework.h
@@ -24,6 +24,7 @@
 #import <FSFramework/FSPasteboardAdditions.h>
 #import <FSFramework/FSFileManagerAdditions.h>
 #import <FSFramework/FSSortDescriptorAdditions.h>
+#import <FSFramework/FSSystem.h>
 
 #import <FSFramework/FSWindowController.h>
 #import <FSFramework/FSViewController.h>
