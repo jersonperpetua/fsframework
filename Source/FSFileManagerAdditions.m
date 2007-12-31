cf53bf8005a4c715e069465ff99e5cbf5680b454
wbyoung <wbyoung@16795141-6145-0410-a428-534aef879a8c>
2007-12-31 00:13:13 +0000

    tagging 0.1.7

--- Source/FSFileManagerAdditions.m
+++ Source/FSFileManagerAdditions.m
@@ -1,5 +1,5 @@
 /* 
- * The FadingRed Shared Framework (FSFramework) is the legal property of its developers, whose names
+ * The Fadingred.org Shared Framework (FSFramework) is the legal property of its developers, whose names
  * are listed in the copyright file included with this source distribution.
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
