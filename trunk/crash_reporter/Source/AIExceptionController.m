79d192e809db50acb4368dbe8e81b5202d6843a2
wbyoung <wbyoung@16795141-6145-0410-a428-534aef879a8c>
2008-05-23 14:47:10 +0000

    fixed the exception controller to not open the crash reporter for caught exceptions

--- trunk/crash_reporter/Source/AIExceptionController.m
+++ trunk/crash_reporter/Source/AIExceptionController.m
@@ -24,6 +24,11 @@
 #define EXCEPTIONS_PATH				[[NSString stringWithFormat:@"~/Library/Logs/CrashReporter/%@.exception.log", \
 	[[NSProcessInfo processInfo] processName]] stringByExpandingTildeInPath]
 
+void AIExceptionHandler(NSException *exception);
+@interface AIExceptionController (PRIVATE)
++ (BOOL)exceptionCatchingEnabled;
+@end
+
 /*
  * @class AIExceptionController
  * @brief Catches application exceptions and forwards them to the crash reporter application
@@ -36,8 +41,9 @@
 //Enable exception catching for the crash reporter
 static BOOL catchExceptions = NO;
 
-//These exceptions can be safely ignored.
-static NSSet *safeExceptionReasons = nil, *safeExceptionNames = nil;
++ (BOOL)exceptionCatchingEnabled {
+	return catchExceptions;
+}
 
 + (void)enableExceptionCatching
 {
@@ -45,90 +51,28 @@ static NSSet *safeExceptionReasons = nil, *safeExceptionNames = nil;
 	NSExceptionHandler *exceptionHandler = [NSExceptionHandler defaultExceptionHandler];
     [exceptionHandler setExceptionHandlingMask:(NSHandleUncaughtExceptionMask |
 												NSHandleUncaughtSystemExceptionMask | 
-												NSHandleUncaughtRuntimeErrorMask |
-												NSHandleTopLevelExceptionMask |
-												NSHandleOtherExceptionMask)];
-	[exceptionHandler setDelegate:self];
+												NSHandleUncaughtRuntimeErrorMask)];
+	NSSetUncaughtExceptionHandler(AIExceptionHandler);
 
 	catchExceptions = YES;
 
 	//Remove any existing exception logs
     [[NSFileManager defaultManager] trashFileAtPath:EXCEPTIONS_PATH];
-
-	//Set up exceptions to except
-	//More of these (matched by substring) can be found in -raise
-	if (!safeExceptionReasons) {
-		safeExceptionReasons = [[NSSet alloc] initWithObjects:
-			@"_sharedInstance is invalid.", //Address book framework is weird sometimes
-			@"No text was found", //ICeCoffEE is an APE haxie which would crash us whenever a user pasted, or something like that
-			@"No URL is selected", //ICeCoffEE also crashes us when clicking links. How obnoxious. Release software should not use NSAssert like this.
-			@"Error (1000) creating CGSWindow", //This looks like an odd NSImage error... it occurs sporadically, seems harmless, and doesn't appear avoidable
-			@"Access invalid attribute location 0 (length 0)", //The undo manager can throw this one when restoring a large amount of attributed text... doesn't appear avoidable
-			@"Invalid parameter not satisfying: (index >= 0) && (index < (_itemArray ? CFArrayGetCount(_itemArray) : 0))", //A couple AppKit methods, particularly NSSpellChecker, seem to expect this exception to be happily thrown in the normal course of operation. Lovely. Also needed for FontSight compatibility.
-			@"Invalid parameter not satisfying: (index >= 0) && (index <= (_itemArray ? CFArrayGetCount(_itemArray) : 0))", //Like the above, but <= instead of <
-			@"Invalid parameter not satisfying: entry", //NSOutlineView throws this, particularly if it gets clicked while reloading or the computer sleeps while reloading
-			@"Invalid parameter not satisfying: aString != nil", //The Find command can throw this, as can other AppKit methods
-			nil];
-	}
-	if (!safeExceptionNames) {
-		safeExceptionNames = [[NSSet alloc] initWithObjects:
-			@"GIFReadingException", //GIF reader sucks
-			@"NSPortTimeoutException", //Harmless - it timed out for a reason
-			@"NSAccessibilityException", //Harmless - one day we should figure out how we aren't accessible, but not today
-			@"NSImageCacheException", //NSImage is silly
-			@"NSArchiverArchiveInconsistency", //Odd system hacks can lead to this one
-			@"NSUnknownKeyException", //No reason to crash on invalid Applescript syntax
-			@"NSObjectInaccessibleException", //We don't use DO, but spell checking does; AppleScript execution requires multiple run loops, and the HIToolbox can get confused and try to spellcheck in the applescript thread. Silly Apple.
-			@"NSCharacterConversionException", //We can't help it if a character can't be converted...
-			nil];
-	}
 }
 
-// mask is NSHandle<exception type>Mask, exception's userInfo has stack trace for key NSStackTraceKey
-+ (BOOL)exceptionHandler:(NSExceptionHandler *)sender shouldHandleException:(NSException *)exception mask:(unsigned int)aMask
-{
-	BOOL		shouldLaunchCrashReporter = YES;
-	if (catchExceptions) {
+@end
+
+void AIExceptionHandler(NSException *exception) {
+
+	BOOL shouldLaunchCrashReporter = YES;
+	if ([AIExceptionController exceptionCatchingEnabled]) {
 		NSString	*theReason = [exception reason];
 		NSString	*theName   = [exception name];
 		NSString	*backtrace = [exception decodedExceptionStackTrace];
 
 		NSLog(@"Caught exception: %@ - %@",theName,theReason);
 		
-		//Ignore various known harmless or unavoidable exceptions (From the system or system hacks)
-		if ((!theReason) || //Harmless
-			[safeExceptionReasons containsObject:theReason] || 
-			[theReason rangeOfString:@"NSRunStorage, _NSBlockNumberForIndex()"].location != NSNotFound || //NSLayoutManager throws this for fun in a purely-AppKit stack trace
-			[theReason rangeOfString:@"Broken pipe"].location != NSNotFound || //libezv throws broken pipes as NSFileHandleOperationException with this in the reason; I'd rather we watched for "broken pipe" than ignore all file handle errors
-			[theReason rangeOfString:@"incomprehensible archive"].location != NSNotFound || //NSKeyedUnarchiver can get confused and throw this; it's out of our control
-			[theReason rangeOfString:@"-whiteComponent not defined"].location != NSNotFound || //Random NSColor exception for certain coded color values
-			[theReason rangeOfString:@"Failed to get fache"].location != NSNotFound || //Thrown by NSFontManager when availableFontFamilies is called if it runs into a corrupt font
-			[theReason rangeOfString:@"NSWindow: -_newFirstResponderAfterResigining"].location != NSNotFound || //NSAssert within system code, harmless
-			[theReason rangeOfString:@"-patternImage not defined"].location != NSNotFound || //Painters Color Picker throws an exception during the normal course of operation.  Don't you hate that?
-			[theReason rangeOfString:@"Failed to set font"].location != NSNotFound || //Corrupt fonts
-			[theReason rangeOfString:@"Delete invalid attribute range"].location != NSNotFound || //NSAttributedString's initWithCoder can throw this
-			[theReason rangeOfString:@"NSMutableRLEArray objectAtIndex:effectiveRange:: Out of bounds"].location != NSNotFound || //-[NSLayoutManager textContainerForGlyphAtIndex:effectiveRange:] as of 10.4 can throw this
-			[theReason rangeOfString:@"TSMProcessRawKeyCode failed"].location != NSNotFound || //May be raised by -[NSEvent charactersIgnoringModifiers]
-			[theReason rangeOfString:@"Invalid PMPrintSettings in print info"].location != NSNotFound || //Invalid saved print settings can make the print dialogue throw this
-			[theReason rangeOfString:@"-[NSConcreteTextStorage attribute:atIndex:effectiveRange:]: Range or index out of bounds"].location != NSNotFound || //Can't find the source of this, but it seems to happen randomly and not provide a stack trace.
-			(!theName) || //Harmless
-			[theName rangeOfString:@"RSS"].location != NSNotFound || //Sparkle's RSS handling whines sometimes, but we don't care.
-		   [safeExceptionNames containsObject:theName])
-		{
-			shouldLaunchCrashReporter = NO;
-		}
-		
-		//Check the stack trace for a third set of known offenders
-		if (!backtrace ||
-			[backtrace rangeOfString:@"-[NSFontPanel setPanelFont:isMultiple:] (in AppKit)"].location != NSNotFound || //NSFontPanel likes to create exceptions
-			[backtrace rangeOfString:@"-[NSScrollView(NSScrollViewAccessibility) accessibilityChildrenAttribute]"].location != NSNotFound || //Perhaps we aren't implementing an accessibility method properly? No idea what though :(
-			[backtrace rangeOfString:@"-[WebBridge objectLoadedFromCacheWithURL:response:data:]"].location != NSNotFound || //WebBridge throws this randomly it seems
-			[backtrace rangeOfString:@"-[NSTextView(NSSharing) _preflightSpellChecker:]"].location != NSNotFound || //Systemwide spell checker gets corrupted on some systems; other apps just end up logging to console, and we should do the same.
-			[backtrace rangeOfString:@"-[NSFontManager(NSFontManagerCollectionAdditions) _collectionsChanged:]"].location != NSNotFound //Deleting an empty collection in 10.4.3 (and possibly other versions) throws an NSRangeException with this in the backtrace.
-		   )
-		{
-			   shouldLaunchCrashReporter = NO;
-		}
+		if (!theReason) { shouldLaunchCrashReporter = NO; }
 			   
 		if (shouldLaunchCrashReporter) {
 			NSString	*bundlePath = [[NSBundle mainBundle] bundlePath];
@@ -140,7 +84,7 @@ static NSSet *safeExceptionReasons = nil, *safeExceptionNames = nil;
 
 			//Pass the exception to the crash reporter and close this application
 			[[NSString stringWithFormat:@"OS Version:\t%@\nLanguage:\t%@\nException:\t%@\nReason:\t%@\nStack trace:\n%@",
-				versionString,preferredLocalization,theName,theReason,backtrace] writeToFile:EXCEPTIONS_PATH atomically:YES];
+				versionString,preferredLocalization,theName,theReason,(backtrace ? backtrace : @"(Unavailable)")] writeToFile:EXCEPTIONS_PATH atomically:YES];
 
 			[[NSWorkspace sharedWorkspace] launchApplication:crashReporterPath];
 
@@ -152,11 +96,7 @@ static NSSet *safeExceptionReasons = nil, *safeExceptionNames = nil;
 				  (backtrace ? backtrace : @"(Unavailable)"));
 		}
 	}
-
-	return shouldLaunchCrashReporter;
 }
-
-@end
 
 @implementation NSException (AIExceptionControllerAdditions)
 //Decode the stack trace within [self userInfo] and return it
@@ -232,3 +172,14 @@ static NSSet *safeExceptionReasons = nil, *safeExceptionNames = nil;
 }
 
 @end
+
+@implementation NSApplication (AIApplicationAdditions)
+
+- (void)reportException:(NSException *)exception {
+	if ([AIExceptionController exceptionCatchingEnabled]) {
+		(*NSGetUncaughtExceptionHandler())(exception);
+	}
+}
+
+@end
+
